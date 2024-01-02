fa65308e2831fe2f5746ecf125852afa7b1e5cec
Raphaël <raphaelamoro330@gmail.com>
2024-01-02 15:35:29 +0100

    update: Footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,21 @@
 <template>
   <footer>
-    <div>
-      <p>Make with ❤️ by Raphaël</p>
-    </div>
-
-    <ul class="nav-links">
-      <li class="nav-links__item"><a href="#">Home</a></li>
-      <li class="nav-links__item"><a href="#">About me</a></li>
-      <li class="nav-links__item"><a href="#">Projects</a></li>
-      <li class="nav-links__item"><a href="#">Contact</a></li>
-    </ul>
+    <p class="notabene">Make with ❤️ by Raphaël</p>
+    <p class="contact"><a href="#">Contact</a></p>
   </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+footer {
+  padding: 10px 0;
+  background-color: #000833;
+  min-height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.contact {
+  font-size: 1.1rem;
+}
+</style>
