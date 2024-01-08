119cd8fd7d551e8736b8202dfffe5aeb3fcf8909
Raphaël <raphaelamoro330@gmail.com>
2024-01-08 14:51:34 +0400

    added: 404 page and router

---
+++ src/views/404.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="not-found">
+    <div class="container">
+      <h1 class="error-code">404</h1>
+      <p class="error-message">Page introuvable.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  min-height: 70vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.container {
+  text-align: center;
+}
+
+.error-code {
+  font-size: 3rem;
+}
+.error-message {
+  font-size: 1.5rem;
+}
+</style>
