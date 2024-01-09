7a18bf7e49294a6ced01228e25737667c95a7d84
Raphaël <raphaelamoro330@gmail.com>
2024-01-09 16:23:46 +0400

    update: use pinia store for projects

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,37 +3,10 @@ import AboutSection from "@/components/Home/AboutSection.vue";
 import ContactSection from "@/components/Home/ContactSection.vue";
 import Header from "@/components/Home/Header.vue";
 import Project from "@/components/Home/Project.vue";
+import { ProjectsStore } from "@/stores/projects.js";
 
-const projects = [
-  {
-    name: "CV",
-    description: "Un CV fait en HTML, Css.",
-    longDescription:
-      "Projet réalisé lors de ma formation au CEF. Nous devions utilisé Html et Css et reproduire la maquette d'un CV.",
-    creationDate: "10 Juin 2023",
-    imagePath: "/img/icon.png",
-    githubRepository: "",
-  },
-  {
-    name: "Espace Commentaire Dynamique",
-    description:
-      "Un espace commentaire dynamique fait avec Html, Css, Javascript.",
-    longDescription:
-      "Projet réalisé lors de ma formation au CEF. Il avait pour but de nous initié à javascript.",
-    creationDate: "10 Juin 2023",
-    imagePath: "/img/icon.png",
-    githubRepository: "",
-  },
-  {
-    name: "Cahier des Charges",
-    description: "Mon tout premier cahier des charges.",
-    longDescription:
-      "Mon tout premier cahier des charges qui est comme tout premier projet, assez médiocre ! Réalisé sur le site Canva.",
-    creationDate: "10 Juin 2023",
-    imagePath: "/img/icon.png",
-    githubRepository: "",
-  },
-];
+const store = ProjectsStore();
+const projects = store.projects;
 </script>
 
 <template>
